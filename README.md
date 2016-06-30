624231ae9f110d59bd08680697e401ef72ad5eda
lmk123 <me@limingkai.cn>
2016-06-30 11:02:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,186 @@
+[![NPM Version](https://img.shields.io/npm/v/namedstorage.svg?style=flat-square)](https://www.npmjs.com/package/namedstorage)
+[![dependencies Status](https://img.shields.io/david/lmk123/namedstorage.svg?style=flat-square)](https://david-dm.org/lmk123/namedstorage)
+[![devDependencies Status](https://img.shields.io/david/dev/lmk123/namedstorage.svg?style=flat-square)](https://david-dm.org/lmk123/namedstorage#info=devDependencies)
+
+# NamedStorage
+
+更高效的使用 `localStorage` 与 `sessionStorage`。
+
+下文统一使用 WebStorage 来表示 `localStorage` 与 `sessionStorage`。
+
+## 特点及使用示例
+
+### JSON 友好
+
+读取与存储的都是 JSON 数据, 这意味着你能保留数据类型:
+
+```js
+const ls = new NamedStorage('local')
+ls.set('key', 1)
+typeof ls.get('key') // "number"
+```
+
+### 命名空间
+
+初始化 NamedStorage 实例时, 你可以提供一个命名空间(**NamedStorage 默认提供了一个命名空间——`"d"`, 即 default 的首字母**), 则对此实例的所有增删改查操作都会以这个命名空间作为前缀, 这能有效的避免冲突:
+
+```js
+const foo = new NamedStorage('local', { name: 'foo' })
+const bar = new NamedStorage('local', { name: 'bar' })
+foo.set('bar', 1)
+bar.set('bar', 2)
+localStorage.getItem('bar') // null
+localStorage.getItem('foo:bar') // "1"
+localStorage.getItem('bar:bar') // "2"
+
+```
+
+相同的存储空间与命名空间会得到同一个实例:
+
+```js
+const ss1 = new NamedStorage('session', { name: 'foo', cache: false })
+const ss2 = new NamedStorage('session', { name: 'foo', cache: true }) // 因为是同一个实例, 所以重新定义 `cache` 与 `lazySave` 配置不会生效
+ss1 === ss2 // true
+
+const ls = new NamedStorage('local', { name: 'foo' })
+ls === ss1 // false
+```
+
+因为有了命名空间的支持, 所以清空此实例的数据时可以选择只清空此命名空间内的数据, 而不是清空整个 WebStorage。
+
+```js
+const ls = new NamedStorage('local', { name: 'foo' })
+ls.set('bar', 1)
+localStorage.getItem('foo:bar') // '1'
+localStorage.setItem('bar', '2')
+
+ls.clear()
+ls.get('bar') // undefined
+localStorage.getItem('foo:bar') // null
+localStorage.getItem('bar') // '2'
+
+ls.clear(true)
+localStorage.getItem('bar') // null
+```
+
+### 缓存数据
+
+相比起每次读取数据时都从 WebStorage 内读取, NamedStorage 能缓存第一次读取的值, 并在后面读取时直接使用缓存值。
+
+### "懒保存"
+
+NamedStorage 提供一个"懒保存"的功能, 如果启用的话, 则每次设置值时不会直接保存到 WebStorage 中, 而是在 `window.onunload` 事件中统一保存:
+
+```js
+const ls = new NamedStorage('local', { lazySave: true })
+ls.set('bar', 'bar')
+localStorage.getItem('d:foo') // null
+window.dispatchEvent(new Event('unload'))
+localStorage.getItem('d:foo') // "\"bar\""
+```
+
+**注意:** 在某些移动设备上 `unload` 事件不一定会触发, 所以请慎用此功能。
+
+## API
+
+### new NamedStorage(type[, options])
+
+### type (String)
+
+"local" 或 "session", 分别对应 `localStorage` 和 `sessionStorage`
+
+### options.cache (Boolean)
+
+默认值为 `true`。设为 `false` 可禁用缓存功能。详情见[缓存数据](#缓存数据)
+
+### options.name (String)
+
+此实例的命名空间, 默认为 `"d"`。
+
+```js
+const ls = new NamedStorage('local', { name: 'hello' })
+ls.namespace // 'hello:' -> 后面多了一个冒号
+```
+
+### options.lazySave (Boolean)
+
+默认值为 `false`。**启用此功能需要启用缓存**。详情见["懒保存"](#"懒保存")。
+
+### 方法
+
+默认值为 `"d"`。详情见[命名空间](#命名空间)。
+
+### NamedStorage.prototype.set(key[, value])
+
+设置数据。如果 `value` 是 `null` 或 `undefined` 则删除这个数据。
+
+### NamedStorage.prototype.get(key)
+
+获取数据。
+
+### NamedStorage.prototype.remove(key)
+
+删除数据。
+
+### NamedStorage.prototype.clear(whole)
+
+默认情况下, 这个方法只会删除当前命名空间内的数据, 但如果指定 `whole` 为 `true`, 则会删除整个 WebStorage 内的数据。详情见[命名空间](#命名空间)。
+
+### 属性
+
+不要去更改这些属性的值, 你应该把这些属性都视为只读的。
+
+### this.storage
+
+当前实例指向的存储空间, `localStorage` 或 `sessionStorage`。
+
+### this.useCache (Boolean)
+
+当前实例是否启用了缓存功能。
+
+### this.caches (Object)
+
+缓存的数据。如果没有启用缓存功能, 则此对象是一个 `undefined`。
+
+### this.name (String)
+
+当前实例的命名空间。
+
+### this.saveOnUnload (Boolean)
+
+当前实例是否启用了["懒保存"](#"懒保存")功能。
+
+## 陷阱
+
+### NamedStorage 提供了一个默认的命名空间——`"d"`
+
+如果你不自定义 `name` 的话, 你每次得到的都是同一个实例:
+
+```js
+const s1 = new NamedStorage('local', { cache: false })
+const s2 = new NamedStorage('local', { cache: true })
+s1 === s2 // true
+s2.namespace === 'd:' // true
+s2.useCache // false
+```
+
+### 如果你启用了缓存, 则直接更改 localStorage 内的值不会反映到 NamedStorage
+
+```js
+const ls = new NamedStorage('local')
+ls.set('foo', 'bar')
+localStorage.getItem('d:foo') // 'bar
+
+localStorage.setItem('d:foo', 'changed')
+ls.get('foo') // 'bar'
+```
+
+推荐统一使用 NamedStorage 操作存储空间。
+
+## TODO
+
+添加单元测试。
+
+## 许可
+
+MIT
---
+++ storage.js
@@ -0,0 +1,198 @@
+/* global define:false */
+(function (global, factory) {
+  var mod = factory(global)
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = mod
+  } else if (typeof define === 'function' && define.amd) {
+    define(function () { return mod })
+  } else {
+    global.NiceStorage = mod
+  }
+}(this, function (global) {
+  'use strict'
+  var jsonParse = JSON.parse
+  var jsonStringify = JSON.stringify
+  var isNull = function (v) {
+    return v === null
+  }
+  var isFunction = function (v) {
+    return typeof v === 'function'
+  }
+  var emptyObj = {}
+  var hasOwn = emptyObj.hasOwnProperty
+  var forIn = function (obj, handler) {
+    for (var key in obj) {
+      if (hasOwn.call(obj, key)) {
+        handler(key)
+      }
+    }
+  }
+
+  /**
+   * 复制 JSON 对象。由于最终保存的是 JSON 字符串,所以不需要使用深复制。
+   * @see https://github.com/lodash/lodash/issues/1984
+   * @param o
+   */
+  var cloneJSON = function (o) {
+    return jsonParse(jsonStringify(o))
+  }
+
+  var allStorages = {
+    local: {},
+    session: {}
+  }
+
+  /**
+   * 构造函数
+   * @param {String} type - "local" 或 "session"。数据应该在哪个 storage 内读取和设置
+   * @param [options]
+   * @param {String} [options.name="d"] - key 的前缀。添加此前缀能避免污染全局 storage 数据
+   * @param {Boolean} [options.cache=true] - 若启用缓存, 则读取数据时会先从缓存内读取,然后从 storage 内读取
+   * @param {Boolean} [options.lazySave=false] - 若启用, 则设置数据时不会立刻写入 storage, 而是在 window.onunload 事件时写入
+   * @constructor
+   */
+  function MyStorage (type, options) {
+    var _options = options || emptyObj
+    var namespace = (_options.name || 'd') + ':'
+
+    var thisType = allStorages[type]
+    // 相同的 type 与 namespace 会获取到同一个实例
+    if (thisType[namespace]) {
+      return thisType[namespace]
+    }
+    thisType[namespace] = this
+
+    this.storage = global[type + 'Storage']
+    this.type = type
+    this.namespace = namespace
+    this.useCache = _options.cache !== false
+    this.saveOnUnload = _options.lazySave && this.useCache && isFunction(global.addEventListener)
+    if (this.useCache) {
+      this.caches = {}
+    }
+    if (this.saveOnUnload) {
+      var that = this
+      global.addEventListener('unload', function () {
+        var caches = that.caches
+        forIn(caches, function (key) {
+          that.storage.setItem(that.getKey(key), jsonStringify(caches[key]))
+        })
+      })
+    }
+  }
+
+  var storageProp = MyStorage.prototype
+
+  /**
+   * 获取实际使用的键名
+   * @param {String} key
+   * @returns {String}
+   */
+  storageProp.getKey = function (key) {
+    return this.namespace + key
+  }
+
+  /**
+   * 设置数据
+   * @param {String} key
+   * @param {*} value
+   */
+  storageProp.set = function (key, value) {
+    // 如果值是 null 或 undefined 则删除这个键
+    if (value == null) {
+      this.remove(key)
+      return
+    }
+    var _key = this.getKey(key)
+
+    if (this.useCache) {
+      this.caches[_key] = cloneJSON(value)
+    }
+    if (!this.saveOnUnload) {
+      this.storage.setItem(_key, jsonStringify(value))
+    }
+  }
+
+  /**
+   * 获取数据
+   * @param {String} key
+   * @returns {*}
+   */
+  storageProp.get = function (key) {
+    var useCache = this.useCache
+    var caches
+
+    // 如果启用缓存,则先从缓存内读取数据
+    if (useCache) {
+      caches = this.caches
+      if (caches.hasOwnProperty(key)) {
+        return cloneJSON(caches[key])
+      }
+    }
+
+    var _key = this.getKey(key)
+    // 尝试从 storage 内读取数据
+    var stringValue = this.storage.getItem(_key)
+    if (isNull(stringValue)) {
+      return
+    }
+
+    // 尝试将 storage 内的数据转换成 json 值
+    var jsonValue
+    try {
+      jsonValue = jsonParse(stringValue)
+    } catch (e) {}
+
+    var value = jsonValue || stringValue
+    if (useCache) {
+      caches[key] = cloneJSON(value)
+    }
+    return value
+  }
+
+  /**
+   * 删除键
+   * @param {String} key
+   */
+  storageProp.remove = function (key) {
+    if (this.useCache) {
+      delete this.caches[key]
+    }
+    if (!this.saveOnUnload) {
+      this.storage.removeItem(this.getKey(key))
+    }
+  }
+
+  /**
+   * 清空数据
+   * @param {Boolean} [whole=false] - 如果为 true,则会清空 storage 内的所有值,否则只清空此命名空间内的值
+   */
+  storageProp.clear = function (whole) {
+    if (whole) {
+      // 清空此 type 下的所有实例的 cache
+      var allTypes = allStorages[this.type]
+      forIn(allTypes, function (key) {
+        var that = allTypes[key]
+        if (that.useCache) {
+          that.caches = {}
+        }
+      })
+      this.storage.clear()
+      return
+    }
+
+    if (that.useCache) {
+      this.caches = {}
+    }
+
+    var namespace = this.namespace
+    var storage = this.stroage
+    forIn(storage, function (key) {
+      if (key.indexOf(namespace) === 0) {
+        storage.removeItem(key)
+      }
+    })
+  }
+
+  return MyStorage
+}))
